0f1c5b3a7370278cbdc2eb5d90be60492c512c75
Przemysław Janiszewski <99132132+przemek890@users.noreply.github.com>
2023-08-26 22:31:31 +0200

    Update Python.swift

--- Front/Front_Main/Front/Python.swift
+++ Front/Front_Main/Front/Python.swift
@@ -4,7 +4,7 @@ func run_python_script(currency: [String], date_start: String, date_end: String)
     let task = Process()
     task.executableURL = URL(fileURLWithPath: "/bin/bash")                                          // system shell used
 
-    let pythonScriptPath = "/Users/przemek899/Desktop/currency_prediction/swift.py"                 // path to swift.py file (don't change its location)
+    let pythonScriptPath = "/Users/przemek899/Desktop/currency_prediction/swift.py"                 // path to swift.py file (The file must be in the main project directory)
     let virtualenvPath = "/Users/przemek899/Desktop/currency_prediction/venv/bin/python"            // path to python in virtual environment
 
     task.currentDirectoryPath = "/Users/przemek899/Desktop/currency_prediction"                     // path to the root directory of the project
